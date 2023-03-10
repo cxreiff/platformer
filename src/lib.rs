d0fa142fa72f923819dab57cab0c048375202f05
cxreiff <dev@cooperreiff.com>
2023-03-10 13:07:38 -0800

    ldtk level import

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 mod config_plugin;
 mod kitty_plugin;
+mod level_plugin;
 mod loading_plugin;
+mod player_plugin;
 
 pub use crate::config_plugin::{
     get_world_position, CameraFlag, ConfigPlugin, ASPECT_RATIO, HEIGHT, WIDTH,
 };
-pub use crate::loading_plugin::{LoadingPlugin, TextureAssets};
+pub use crate::loading_plugin::{LoadingPlugin, AllAssets};
 
 use crate::kitty_plugin::KittyPlugin;
+use crate::level_plugin::LevelPlugin;
+use crate::player_plugin::PlayerPlugin;
 use bevy::prelude::{App, Plugin};
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
@@ -20,6 +24,9 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(LoadingPlugin).add_plugin(KittyPlugin);
+        app.add_plugin(LevelPlugin)
+            .add_plugin(LoadingPlugin)
+            .add_plugin(KittyPlugin)
+            .add_plugin(PlayerPlugin);
     }
 }
